c77643ae6240a03564e3fe2416cafd888126ed46
Denys Chernyshov <den.blackshov@gmail.com>
2024-04-16 22:03:55 +0200

    Add extra utilities for Results

--- utils/src/main/kotlin/io/github/coden/utils/Results.kt
+++ utils/src/main/kotlin/io/github/coden/utils/Results.kt
@@ -4,11 +4,12 @@ import com.andreapivetta.kolor.Color
 import org.apache.logging.log4j.kotlin.KotlinLogger
 
 
-fun <T> Result<T>.logInteraction(logger: KotlinLogger, operation: (T) -> String): Result<T> {
+fun <T> Result<T>.logResult(logger: KotlinLogger, operation: (T) -> String): Result<T> {
     return this
         .onSuccess { logger.info("${operation(it)} - ${Color.LIGHT_GREEN("Success!")}") }
         .onFailure { logger.error("${Color.LIGHT_RED("Failed!")} - " + it.message, it) }
 }
+
 
 fun <T : Any> T.success(): Result<T> = Result.success(this)
 fun success(): Result<Unit> = Unit.success()
@@ -26,16 +27,68 @@ inline fun <reified T : Any> T?.notNullOrFailure(): Result<T> {
         ?: Result.failure(IllegalArgumentException("Element was not found ${T::class.simpleName}"))
 }
 
-inline fun <R, T> Result<T>.flatMap(transform: (value: T) -> Result<R>): Result<R> {
-    return this.mapCatching { transform(it).getOrThrow() }
+fun <R, T> Result<T>.flatMap(transform: (T) -> Result<R>): Result<R> {
+    fold(
+        { return transform(it) },
+        { return Result.failure(it) }
+    )
 }
 
-inline fun <reified E, R> Result<R>.recover(transform: (exception: E) -> R): Result<R> {
+fun <T, R> Result<T>.combine(transform: (T) -> Result<R>): Result<Pair<T, R>> {
+    fold(
+        { first -> return transform(first).map { second -> first to second } },
+        { return Result.failure(it) }
+    )
+}
+
+fun <T, R, E> Result<Pair<T,R>>.combine(transform: (T, R) -> Result<E>): Result<Triple<T, R, E>> {
+    fold(
+        { (f, s) -> return transform(f, s).map { third -> Triple(f, s, third) } },
+        { return Result.failure(it) }
+    )
+}
+
+fun <T, R> Result<T>.concat(second: Result<R>): Result<Pair<T, R>> {
+    return concatParallel { second }
+}
+
+fun <T, R> Result<T>.concat(supply: () -> Result<R>): Result<Pair<T, R>> {
+    fold(
+        {f -> supply().fold(
+            {s -> return Result.success(f to s)},
+            {s -> return Result.failure(s) }
+        )},
+        {f ->  return Result.failure(f)},
+        )
+
+}
+
+fun <T, R> Result<T>.concatParallel(second: Result<R>): Result<Pair<T, R>> {
+    return concatParallel { second }
+}
+
+fun <T, R> Result<T>.concatParallel(supply: () -> Result<R>): Result<Pair<T, R>> {
+    val second = supply()
+    fold(
+        {f -> second.fold(
+            {s -> return Result.success(f to s)},
+            {s -> return Result.failure(s) }
+        )},
+        {f -> second.fold(
+            {s -> return Result.failure(f)},
+            {s -> return Result.failure(ThrowableAggregate(f, s)) }
+        )}
+    )
+}
+
+class ThrowableAggregate(vararg val exceptions: Throwable) :
+    Exception("Throwable Aggregate:\n" + exceptions.joinToString("\n"))
+
+inline fun <reified E, R> Result<R>.recover(transform: (E) -> R): Result<R> {
     return this.recoverCatching {
-        if (it is E){
+        if (it is E) {
             return@recoverCatching transform(it)
-        }
-        else {
+        } else {
             throw it
         }
     }
