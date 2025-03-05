0cef53dc7753e09ba4f32a364f64d5b28e3dfef1
Denys Chernyshov <den.blackshov@gmail.com>
2025-03-05 20:45:23 +0100

    update modules

--- telegram/src/main/kotlin/io/github/coden/telegram/abilities/BaseTelegramBot.kt
+++ telegram/src/main/kotlin/io/github/coden/telegram/abilities/BaseTelegramBot.kt
@@ -1,6 +1,5 @@
 package io.github.coden.telegram.abilities
 
-import io.github.coden.telegram.commands.replyOnCallbackCommand
 import io.github.coden.telegram.db.BotDB
 import org.telegram.abilitybots.api.bot.AbilityBot
 import org.telegram.abilitybots.api.objects.Ability
@@ -10,33 +9,42 @@ import org.telegram.telegrambots.meta.api.objects.ChatJoinRequest
 import org.telegram.telegrambots.meta.api.objects.Update
 
 open class BaseTelegramBot<D : BotDB>(
-    protected  val config: TelegramBotConfig,
+    protected val config: TelegramBotConfig,
     private val botDB: D,
     options: DefaultBotOptions = optionsOf()
-) : AbilityBot(config.token, config.username, botDB, options), RunnableLongPollingBot{
+) : AbilityBot(config.token, config.username, botDB, options), RunnableLongPollingBot {
 
     protected open val db = botDB
     override fun db(): D = botDB
 
-    override fun creatorId(): Long { return config.target }
+    override fun creatorId(): Long {
+        return config.target
+    }
 
     override fun run() {
         greet()
         sendDebugUpdates()
     }
 
-    open fun sendDebugUpdates() { onUpdatesReceived(debugUpdates()) }
-    open fun debugUpdates(): List<Update>{ return listOf() }
+    open fun sendDebugUpdates() {
+        onUpdatesReceived(debugUpdates())
+    }
+
+    open fun debugUpdates(): List<Update> {
+        return listOf()
+    }
 
     open fun start(): Ability = ability("start") { greet() }
-    open fun greet() { silent.sendMd(config.intro, config.target) }
+    open fun greet() {
+        silent.sendMd(config.intro, config.target)
+    }
 
     override fun onUpdateReceived(update: Update?) {
         // library does not see a valid user on reactions
         // hack to force library to think it has a valid user, but supplying fake info
         // it'll return EMPTY_USER
         // we'll sacrifice chatjoin types of messages
-        if (update?.messageReaction != null){
+        if (update?.messageReaction != null) {
             update.chatJoinRequest = ChatJoinRequest().apply { user = EMPTY_USER }
         }
         super.onUpdateReceived(update)
